25b7aedf5c5fbd1f5ad9b98fbb17a51f6ada7d92
raphaelgyo <57181994+raphaelgyo@users.noreply.github.com>
2021-02-01 20:18:08 +0100

    correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,55 @@
-// Appeler l'heure locale
-clock();
+// calcul la rotation de chaque aiguille en fonction de son nom et de l'heure
+function rotateHand(name, time) {
+    // Récupère l'aiguille ciblée par name
+    let Hand = document.querySelector(`.${name}`);
 
-// fonction pour afficher l'heure
-function clock() {
-    // Date
-    const date = new Date();
-    const hours = ((date.getHours() + 11) % 12) + 1;
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
+    // fixe la division par défaut pour le cadran : il y a 60 minutes dans une heure, 60 secondes dans une minute
+    let dialDivisions = 60;
 
-    // Degrés
-    const hour = hours * 30;
-    const minute = minutes * 6;
-    const second = seconds * 6;
+    // remplace la valeur par défaut si on travaille sur les heures: il y a 12 heures sur le cadran d'une montre
+    if (name === 'hours') {
+        dialDivisions = 12;
+    }
 
-    // Afficher
-    document.querySelector('.heure').style.transform = `rotate(${hour}deg)`;
+    //calcule le degré de rotation : 180 = par de rotation, on reste sur 12h
+    let rotationDeg = 180 + (time / dialDivisions) * 360;
 
-    document.querySelector('.minute').style.transform = `rotate(${minute}deg)`;
-
-    document.querySelector('.seconde').style.transform = `rotate(${second}deg)`;
+    // modifie le css avec la valeur du transform.
+    Hand.style.transform = `rotate(${rotationDeg}deg)`;
 }
 
-// Interval
-setInterval(clock, 1000);
+// lancée au chargement. appelle la fonction rotateHand chaque seconde, sur les 3 aiguilles.
+function init() {
+    let interval = setInterval(() => {
+        let time = new Date();
+
+        rotateHand('hours', time.getHours());
+        rotateHand('minutes', time.getMinutes());
+        rotateHand('seconds', time.getSeconds());
+    }, 1000);
+}
+
+init();
+
+function drawMarker(place, className) {
+    let marker = document.createElement('div');
+    marker.classList.add(className);
+    return place.appendChild(marker);
+}
+
+function drawMarkers() {
+    let frame = document.querySelector('.wrapper');
+
+    for (i = 0; i < 60; i++) {
+        let className = 'marker';
+
+        if (i % 5 === 0) {
+            className = 'mainMarker';
+        }
+
+        let marker = drawMarker(frame, className);
+        marker.style.transform = `rotate( ${180 + (i / 60) * 360}deg )`;
+    }
+}
+
+drawMarkers();
